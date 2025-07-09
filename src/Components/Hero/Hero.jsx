9ba9471e6d32b3c17257190b75d2294670cc27cc
Araba <araba@example.com>
2025-07-09 22:58:03 +0800

    Describe what you changed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
 import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+import SplitText from '../../../Reactbits/SplitText/SplitText';
+import ShinyText from '../../../Reactbits/ShinyText/ShinyText';
+
+const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+};
 
 const Hero = () => {
   return (
     <div className='hero'>
-      <img src="" alt="" />
-      <h1>I'm Arragen Basilio, frontend developer based in Philippines</h1>
+      <img src={profile_img} alt="" />
+      <SplitText
+        text="I'm Arragen Basilio"
+        className="text-2xl font-semibold text-center"
+        delay={100}
+        duration={0.7}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+        onLetterAnimationComplete={handleAnimationComplete} />
       <p>Seeking a career with a progressive organization that provides an opportunity to
         capitalize my technical skills and abilities in the field of Information Technology.</p>
-        <div className="hero-action">
-          <div className="hero-connect">Connect with me</div>
-          <div className="hero-resume">My Resume</div>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <ShinyText text="Connect with me" disabled={false} speed={2} className='custom-class' />
         </div>
+        <div className="hero-resume">
+          <ShinyText text="My Resume" disabled={false} speed={2} className='custom-class' />
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
+import ShinyText from './../../../Reactbits/ShinyText/ShinyText';
+import GradientText from './../../../Reactbits/GradientText/GradientText';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={logo} alt="" />
-        <ul className="nav-menu">
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-        </ul>
-        <div className="nav-connect">Connect with me</div>
+      <img src={logo} alt="" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">
+        <GradientText
+          colors={["#c3fc3c", "#3cfc4c", "#8efc44", "#40fc50", "#c3fc3c"]}
+          animationSpeed={3}
+          showBorder={false}
+          className="custom-class"
+        >
+          Connect with me
+        </GradientText>
+      </div>
     </div>
   )
 }

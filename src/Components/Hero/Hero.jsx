81505a8cf8e394075dc53a9b39e6f997dc4ed7d2
Araba <araba@example.com>
2025-07-09 20:14:07 +0800

    Describe what you changed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <img src="" alt="" />
+      <h1>I'm Arragen Basilio, frontend developer based in Philippines</h1>
+      <p>Seeking a career with a progressive organization that provides an opportunity to
+        capitalize my technical skills and abilities in the field of Information Technology.</p>
+        <div className="hero-action">
+          <div className="hero-connect">Connect with me</div>
+          <div className="hero-resume">My Resume</div>
+        </div>
+    </div>
   )
 }
 
